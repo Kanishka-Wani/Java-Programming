bf285515c7693c95d86a7a2832d9a6f343877a8e
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-24 21:32:18 +0530

    Add practice.java with variable declarations and prints

---
+++ Day_02/practice.java
@@ -0,0 +1,18 @@
+public class practice{
+    public static void main(String[] args){
+        int age =20;
+        double cgpa=8.56;
+        char grade ='A';
+        boolean passed = true;
+
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(grade);
+        System.out.println(passed);
+
+        int marks =90;
+        System.out.println(marks);
+        marks=95;
+        System.out.println(marks);
+    }
+}
