ce80aa19dae0dcc478f5864f641117bb45d1b9a9
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-25 22:24:58 +0530

    Add ConvertMeter class to convert kilometers to meters

---
+++ ConvertMeter.java
@@ -0,0 +1,10 @@
+public class ConvertMeter {
+    public static void main(String[] args){
+        double distance = 255; // in kilometers
+
+        double mdistance = distance * 1000.0; // in meters
+
+        System.out.println("Distance in meters is : "+ mdistance);
+
+    }
+}
