eef624d0636a803061c873d7d48b37ed8be7f817
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-25 22:27:11 +0530

    Add comment for program purpose in Perimeter.java
    Add a comment indicating the purpose of the program.

---
+++ Perimeter.java
@@ -0,0 +1,20 @@
+//Write a program to find perimeter of Rectangle
+
+import java.util.Scanner;
+
+public class Perimeter{
+    public static void main(String[] args){
+      Scanner sc= new Scanner(System.in);
+
+      System.out.println("Enter the length : ");
+      double length = sc.nextDouble();
+      System.out.println("Enter width: ");
+      double width = sc.nextDouble();
+
+      double perimeter = 2 *(length + width) ;
+
+      System.out.println("Perimeter of rectangle is : " + perimeter);
+
+      sc.close();
+  }
+}
