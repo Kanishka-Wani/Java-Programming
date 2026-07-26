3b3758f836fd155255d2319d871b955f4af75767
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-26 22:44:08 +0530

    Print your name 10 times
    Updated class name from 'names' to 'Names' and added a comment.

---
+++ Day_04/Names.java
@@ -0,0 +1,11 @@
+//Print your name 10 times
+
+public class Names{
+    public static void main(String[] args){
+        String nm= "Kanishka";
+
+        for(int i =0; i<10; i++){
+            System.out.println(nm);
+        }
+    }
+}
