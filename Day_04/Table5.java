88a82ea928624915b96940f891af22a29d7532bb
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-26 22:52:07 +0530

    print multiplication table of 5

---
+++ Day_04/Table5.java
@@ -0,0 +1,8 @@
+//Table of 5
+public class Table5{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("5 x "+  i +" = "+ (5*i));
+        }
+    }
+}
