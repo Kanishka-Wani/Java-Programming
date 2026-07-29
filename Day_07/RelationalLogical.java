0d7d91342b9ad411307a4ddb8c50939154939781
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-07-29 22:54:48 +0530

    Add RelationalLogical class with logical operations

---
+++ Day_07/RelationalLogical.java
@@ -0,0 +1,9 @@
+public class RelationalLogical{
+  public static void main(String[] args){
+    int age =22;
+    Boolean citizen = true;
+   System.out.println(age>=18 && citizen);
+   System.out.println(age<18 || citizen);
+   System.out.println(!citizen);
+}
+}
